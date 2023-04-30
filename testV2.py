d21e8ff8d7c9e0cf47f38544274d47cd21d8ebbe
TekhEx2 <techsoldierex3@gmail.com>
2023-04-29 20:43:49 -0700

    monthlycalculator

--- testV2.py
+++ testV2.py
@@ -5,6 +5,11 @@ b = 231
 
 print(a*b)
 
-total = test.morgagecalculator(30,0.04,400000,12)
-print("this will be the total amount you owe" + total)
+total = test.morgagecalculator(30,0.035,300000,12)
+print(f"this will be the total amount you owe {total:,.2f}" )
+def monthlyPayment(total,years):
+    return(total/(years*12))
 
+monthly_payment = monthlyPayment(total,30)
+
+print(f"this will be the amount you pay per month {monthly_payment:,.2f}")
